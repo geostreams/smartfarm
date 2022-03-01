7b048c45381d56d393da8a6cbe4cb31e08032ca4
Diego Calderon <diegoac2@illinois.edu>
2022-03-01 11:39:10 -0600

    updated ids

--- src/config.js
+++ src/config.js
@@ -27,19 +27,13 @@ export default {
                     title: 'SR 09-30-2020',
                     id: 'manual:SatelliteImage.Reifsteck.2020.09.30.SR',
                     type: 'wms',
-                    legendImage: '/wms?request=GetMap&' +
-                'layers=manual:SatelliteImage.Reifsteck.2020.09.30.SR&' +
-                'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                '&width=640&height=480&srs=EPSG:3857&format=image%2Fpng'
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:research-region&format=image%2Fpng'
                 },
                 {
                     title: 'SR 09-01-2020',
                     id: 'manual:SatelliteImage.Reifsteck.2020.09.01.SR',
                     type: 'wms',
-                    legendImage: '/wms?request=GetMap&' +
-                'layers=manual:SatelliteImage.Reifsteck.2020.09.01.SR&' +
-                'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                '&width=640&height=480&srs=EPSG:3857&format=image%2Fpng'
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:research-region&format=image%2Fpng'
                 }
             ],
             'Satellite Multispectral Image NDVI': [
@@ -47,239 +41,107 @@ export default {
                     title: 'NDVI 09-30-2020',
                     id: 'manual:SatelliteImage.Reifsteck.2020.09.30.NDVI',
                     type: 'wms',
-                    legendImage: '/wms?request=GetMap&' +
-                'layers=manual:SatelliteImage.Reifsteck.2020.09.30.NDVI&' +
-                'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                '&width=640&height=480&srs=EPSG:3857&format=image%2Fpng'
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:research-region&format=image%2Fpng'
                 },
                 {
                     title: 'NDVI 09-01-2020',
                     id: 'manual:SatelliteImage.Reifsteck.2020.09.01.NDVI',
                     type: 'wms',
-                    legendImage: '/wms?request=GetMap&' +
-                'layers=manual:SatelliteImage.Reifsteck.2020.09.01.NDVI&' +
-                'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:research-region&format=image%2Fpng'
                 }
             ],
             'Cation Exchange Capacity (meg/100g)':[
                 {
                     title: 'T_CEC_meq_100g_RS_2020',
-                    id: 'T_CEC_meq_100g_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:T_CEC_meq_100g_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:T_CEC_meq_100g_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:T_CEC_meq_100g_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'M_CEC_meq_100g_RS_2020',
-                    id: 'M_CEC_meq_100g_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:M_CEC_meq_100g_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:M_CEC_meq_100g_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:M_CEC_meq_100g_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'B_CEC_meq_100g_RS_2020',
-                    id: 'B_CEC_meq_100g_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:B_CEC_meq_100g_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:B_CEC_meq_100g_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:B_CEC_meq_100g_RS_2020&format=image%2Fpng'
                 }
             ],
             'Moisture Content (g/g)':[
                 {
                     title: 'T_MC_g_g_RS_2020',
-                    id: 'T_MC_g_g_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:T_MC_g_g_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:T_MC_g_g_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:T_MC_g_g_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'M_MC_g_g_RS_2020',
-                    id: 'M_MC_g_g_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:M_MC_g_g_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:M_MC_g_g_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:M_MC_g_g_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'B_MC_g_g_RS_2020',
-                    id: 'B_MC_g_g_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:B_MC_g_g_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:B_MC_g_g_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:B_MC_g_g_RS_2020&format=image%2Fpng'
                 }
             ],
             'Organic Matter (%)':[
                 {
                     title: 'T_OM_RS_2020',
-                    id: 'T_OM_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:T_OM_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:T_OM_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:T_OM_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'M_OM_RS_2020',
-                    id: 'M_OM_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:M_OM_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:M_OM_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:M_OM_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'B_OM_RS_2020',
-                    id: 'B_OM_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:B_OM_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:B_OM_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:B_OM_RS_2020&format=image%2Fpng'
                 }
             ],
             'Soil Organic Carbon (%)':[
                 {
                     title: 'T_SOC_RS_2020',
-                    id: 'T_SOC_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:T_SOC_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:T_SOC_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:T_SOC_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'M_SOC_RS_2020',
-                    id: 'M_SOC_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:M_SOC_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:M_SOC_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:M_SOC_RS_2020&format=image%2Fpng'
                 }
             ],
             'Soil pH':[
                 {
                     title: 'T_SoilpH_RS_2020',
-                    id: 'T_SoilpH_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:T_SoilpH_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:T_SoilpH_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:T_SoilpH_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'M_SoilpH_RS_2020',
-                    id: 'M_SoilpH_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:M_SoilpH_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:M_SoilpH_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:M_SoilpH_RS_2020&format=image%2Fpng'
                 },
                 {
                     title: 'B_SoilpH_RS_2020',
-                    id: 'B_SoilpH_RS_2020',
-                    wms: `${process.env.GEOSERVER_URL}/wms`,
-                    opacity: 0.80,
-                    visibility: false,
-                    legendShow: true,
-                    legendStartOpen: false,
-                    legendTitle: 'Opacity',
-                    legendText: '',
-                    legendImage: '/wms?request=GetMap&' +
-                        'layers=manual:B_SoilpH_RS_2020&' +
-                        'bbox=-9828974.37528645,4866229.991661974,-9827597.817471862,4867541.604250039' +
-                        `&width=640&height=480&srs=EPSG:3857&format=image%2Fpng`
+                    id: 'manual:B_SoilpH_RS_2020',
+                    type: 'wms',
+                    legendImage: '/wms?request=GetLegendGraphic&layer=manual:B_SoilpH_RS_2020&format=image%2Fpng'
                 }
             ]
         }
